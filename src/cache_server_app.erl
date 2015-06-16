429505012380109d7409aac3fe57d53e4d1f600d
Vitaliy Serakov <v.serakov@favbet.com>
2015-06-16 13:20:50 +0300

    move start cowboy and ranch to application

--- src/cache_server_app.erl
+++ src/cache_server_app.erl
@@ -10,7 +10,34 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    cache_server_sup:start_link().
+	case get_env(tcp_api_enabled, false) of
+		false -> do_nothing;
+		true ->
+			TcpPort = get_env(tcp_api_port, 5555),
+			{ok, _} = ranch:start_listener(tcp_api, 100, ranch_tcp, [{port, TcpPort}], cache_server_tcp, []);
+		_ -> exit(wrong_config)
+	end,
+	case get_env(http_api_enabled, false) of
+		false -> do_nothing;
+		true ->
+			HttpPort = get_env(http_api_port, 8080),
+			Dispatch = cowboy_router:compile([
+											  {'_', [{"/", cache_server_http, []},
+													 {"/api/cache_server", cache_server_http, []}]}
+											 ]),
+			{ok, _} = cowboy:start_http(my_http_listener, 100, [{port, HttpPort}],
+										[{env, [{dispatch, Dispatch}]}]
+									   );
+		_ -> exit(wrong_config)
+	end,
+	
+	cache_server_sup:start_link().
 
 stop(_State) ->
-    ok.
+	ok.
+
+get_env(Key, Def) ->
+	case application:get_env(cache_server, Key) of
+		{ok, Val} -> Val;
+		undefined -> Def
+	end.
--- src/cache_server_sup.erl
+++ src/cache_server_sup.erl
@@ -24,34 +24,6 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-	case get_env(tcp_api_enabled, false) of
-		false -> do_nothing;
-		true ->
-			TcpPort = get_env(tcp_api_port, 5555),
-			{ok, _} = ranch:start_listener(tcp_api, 100, ranch_tcp, [{port, TcpPort}], cache_server_tcp, []);
-		_ -> exit(wrong_config)
-	end,
-	case get_env(http_api_enabled, false) of
-		false -> do_nothing;
-		true ->
-			HttpPort = get_env(http_api_port, 8080),
-			Dispatch = cowboy_router:compile([
-											  {'_', [{"/", cache_server_http, []},
-													 {"/api/cache_server", cache_server_http, []}]}
-											 ]),
-			{ok, _} = cowboy:start_http(my_http_listener, 100, [{port, HttpPort}],
-										[{env, [{dispatch, Dispatch}]}]
-									   );
-		_ -> exit(wrong_config)
-	end,
-	
 	{ok, { {one_for_one, 5, 10}, [
 								  ?CHILD(cache_server_srv, worker)
 								 ]} }.
-
-
-get_env(Key, Def) ->
-	case application:get_env(cache_server, Key) of
-		{ok, Val} -> Val;
-		undefined -> Def
-	end.
